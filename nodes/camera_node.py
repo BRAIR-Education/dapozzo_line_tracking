b257285b4b1ebbeb28e31c700795460ffec05600
Leonardo Da Pozzo <l.dapozzo@studenti.unipi.it>
2023-07-12 01:05:25 +0200

    alternative, simpler camera node impl using centroids

--- nodes/camera_node.py
+++ nodes/camera_node.py
@@ -21,12 +21,9 @@ LOWER_YELLOW = (0, 10, 10)
 UPPER_YELLOW = (40, 255, 255)
 
 # Colors expressed in BGR format
-TRACK_OUTLINE_COLOR = (255, 255, 255)
-LEFT_LIMIT_COLOR = (255, 150, 0)
-RIGHT_LIMIT_COLOR = (0, 255, 255)
-CENTERLINE_COLOR = (0, 255, 0)
+TRACK_OUTLINE_COLOR = (255, 0, 255)
 CROSSHAIR_COLOR = (255, 255, 255)
-WAYPOINT_COLOR = (255, 255, 255)
+CENTROID_COLOR = (255, 255, 255)
 ERROR_COLOR = (0, 0, 255)
 
 
@@ -56,136 +53,54 @@ class CameraNode:
 
         image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         height, width, _ = image.shape
+        image = image[int(height / 2) : height, 200 : width - 200]
+        height, width, _ = image.shape
 
-        track_outline = self.get_track_outline(image)
+        # Convert to HSV and threshold the image to extract the (yellow) track
+        hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        mask = cv.inRange(hsv, np.array(LOWER_YELLOW), np.array(UPPER_YELLOW))
 
-        # Crop image to remove unwanted border pixels and to ignore faraway parts of the track
-        cropped_outline = track_outline[
-            int(height / 2) : (height - 10), 10 : (width - 10)
-        ]
-        cr_height, cr_width = cropped_outline.shape
+        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        left_limit, right_limit = self.extract_track_limits(cropped_outline)
-        centerline = self.compute_centerline(left_limit, right_limit)
-
-        if left_limit.size == 0 or right_limit.size == 0:
-            rospy.logwarn("WARNING: empty track limit")
-            return
+        # Compute centroid
+        M = cv.moments(contours[0])
+        if M["m00"] != 0:
+            centroid_x = int(M["m10"] / M["m00"])
+            centroid_y = int(M["m01"] / M["m00"])
+        else:
+            centroid_x, centroid_y = 0, 0
 
         # Compute crosshair
-        center_x = math.floor(cr_width / 2)
-        center_y = math.floor(cr_height / 2)
+        center_x = math.floor(width / 2)
+        center_y = math.floor(height / 2)
 
-        # Detect waypoint (centerline point closest to crosshair)
-        waypoint, waypoint_offset = self.get_next_waypoint(
-            centerline, (center_x, center_y)
-        )
-
-        # Publish x-axis offset between the projected waypoint and the crosshair
+        # Publish x-axis offset between the centroid and the crosshair
         msg = std_msgs.msg.Float32()
-        msg.data = waypoint_offset
+        msg.data = centroid_x - center_x
         self.error_pub.publish(msg)
 
         if self.viz:
             self.display_data(
-                left_limit,
-                right_limit,
-                centerline,
+                image,
+                contours,
+                (centroid_x, centroid_y),
                 (center_x, center_y),
-                waypoint,
-                cr_height,
-                cr_width,
             )
 
         # end = time.time()
         # rospy.loginfo(end - start)
 
-    # Detect the track in the input image, draw its contour on a new binary image and return it
-    def get_track_outline(self, input):
-        height, width, _ = input.shape
-        track_outline = np.zeros((height, width), dtype=np.uint8)
-
-        # Convert to HSV and threshold the image to extract the (yellow) track
-        hsv = cv.cvtColor(input, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv, np.array(LOWER_YELLOW), np.array(UPPER_YELLOW))
-
-        # Detect track outline and draw it on a new image
-        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        cv.drawContours(track_outline, contours, 0, TRACK_OUTLINE_COLOR)
-
-        return track_outline
-
-    # Compute the centerline given the left and right track limits and return it
-    def compute_centerline(self, left, right):
-        centerline = []
-        for (x1, y1), (x2, y2) in list(zip(left, right)):
-            xc = math.floor((x1 + x2) / 2)
-            yc = math.floor((y1 + y2) / 2)
-
-            centerline.append((xc, yc))
-
-        return np.array(centerline)
-
-    # Return the left and right track limits from the provided track outline
-    def extract_track_limits(self, track_outline):
-        # We expect 3 labels:
-        #   0: background
-        #   1: left track limit
-        #   2: right track limit
-        _, labels = cv.connectedComponents(track_outline)
-
-        left_limit_cols, left_limit_rows = np.where(labels == 1)
-        left_limit = np.column_stack((left_limit_rows, left_limit_cols))[::10]
-
-        right_limit_cols, right_limit_rows = np.where(labels == 2)
-        right_limit = np.column_stack((right_limit_rows, right_limit_cols))[::10]
-
-        return left_limit, right_limit
-
-    # Obtain the next waypoint based on crosshair position
-    #   and return it along with its x-axis offset
-    def get_next_waypoint(self, trajectory, crosshair):
-        if trajectory.size == 0:
-            return crosshair, 0
-
-        center_x, center_y = crosshair
-
-        closest = 0
-        closest_dist = float("inf")
-        for i, (x, y) in enumerate(trajectory):
-            # Ignore waypoints below crosshair
-            if y > center_y - 30:
-                continue
-
-            dist = math.sqrt(abs(x - center_x) ** 2 + abs(y - center_y) ** 2)
-            if dist < closest_dist:
-                closest_dist = dist
-                closest = i
-
-        return trajectory[closest], x - center_x
-
     # Visualize data for debugging purposes
-    def display_data(
-        self, left_limit, right_limit, centerline, crosshair, waypoint, height, width
-    ):
-        canvas = np.zeros((height, width, 3), dtype=np.uint8)
+    def display_data(self, image, contours, centroid, crosshair):
+        cv.drawContours(image, contours, -1, TRACK_OUTLINE_COLOR, 2)
 
-        for point in left_limit:
-            cv.circle(canvas, point, 1, LEFT_LIMIT_COLOR, 1)
+        cv.circle(image, centroid, 5, CENTROID_COLOR, 2)
 
-        for point in right_limit:
-            cv.circle(canvas, point, 1, RIGHT_LIMIT_COLOR, 1)
+        cv.circle(image, crosshair, 5, CROSSHAIR_COLOR, 2)
 
-        for point in centerline:
-            cv.circle(canvas, point, 1, CENTERLINE_COLOR, 1)
+        cv.line(image, centroid, crosshair, ERROR_COLOR, 2)
 
-        cv.circle(canvas, crosshair, 3, CROSSHAIR_COLOR)
-
-        cv.circle(canvas, waypoint, 3, WAYPOINT_COLOR)
-
-        cv.line(canvas, crosshair, waypoint, ERROR_COLOR, 1)
-
-        cv.imshow("Visualization", canvas)
+        cv.imshow("Visualization", image)
         cv.waitKey(1)
 
 
